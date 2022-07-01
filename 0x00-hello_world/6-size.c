f3606be149b03215bbafd99817494d2e1dab5178
isaactls <isacktolesa@gmail.com>
2022-07-01 13:40:15 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-pritnf("Size of a char: " sizeof(char));
-printf("Size of a int: " sizeof(int));
-printf("Size of a long int: " sizeof(long int));
-printf("Size of a long long int: " sizeof(long long int));
-printg("Size of a float: " sizeof(float));
+pritnf("Size of a char: ", sizeof(char));
+printf("Size of a int: ", sizeof(int));
+printf("Size of a long int: ", sizeof(long int));
+printf("Size of a long long int: ", sizeof(long long int));
+printg("Size of a float: ", sizeof(float));
 return (0);
 }
