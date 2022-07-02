67502e247fc2d9c8b649939d627040d9e8dc5444
isaactls <isacktolesa@gmail.com>
2022-07-02 12:32:21 -0700

    gg

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,12 +19,12 @@ for (a = 48; a <= 57; a++)
 for (b = 49; b <= 57; b++)
 {
 for (c = 50; c <= 57; c++)
-if (b > a && a > c)
+if (b > a && c > b)
 {
 putchar(a);
 putchar(b);
 putchar(c);
-if (b != 56 || c != 57)
+if (a != 56 || b != 57)
 {
 putchar(44);
 putchar(32);
