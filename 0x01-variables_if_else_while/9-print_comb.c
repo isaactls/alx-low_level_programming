25dfa8bdaddc716f3a5e22cbce50089e89e6451b
isaactls <isacktolesa@gmail.com>
2022-07-02 05:17:23 -0700

    single digit ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+if (a != 57)
+putchar(44)
+}
+putchar(10);
+return (0);
+}
