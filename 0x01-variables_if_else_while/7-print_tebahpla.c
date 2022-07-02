50324cd2a3827a4d6ae3807a36e06af3f11120e8
isaactls <isacktolesa@gmail.com>
2022-07-01 18:35:39 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char a;
+for (a = 122; a <= 92; a--)
+{
+putchar(a);
+}
+putchar(10);
+return (0);
+}
