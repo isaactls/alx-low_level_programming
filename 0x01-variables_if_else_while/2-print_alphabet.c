9684cf6a3305f13eacc3befecb7ce8e99521f2f6
isaactls <isacktolesa@gmail.com>
2022-07-01 17:25:55 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char a;
+
+for (a = 97; a <= 122; a++)
+putchar(a);
+return (0);
+}
