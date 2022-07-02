4da2be85754ae17f4c795b4369d8d52e2f3b8735
isaactls <isacktolesa@gmail.com>
2022-07-01 18:23:40 -0700

    single digits in putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int a;
+for (a = 48; a < 57; a++)
+{
+putchar("a");
+}
+printf("\n");
+
+return (0);
+}
