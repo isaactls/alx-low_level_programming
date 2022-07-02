e1f24d7e13578c062bd61c011d1239f6654f699b
isaactls <isacktolesa@gmail.com>
2022-07-01 18:17:11 -0700

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int a;
+for (a = 0; a < 10; a++)
+printf(a);
+
+
+return (0);
+}
