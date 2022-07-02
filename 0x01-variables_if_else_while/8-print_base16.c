96be622742fd7d4ef8d8c62030c219156fa723ca
isaactls <isacktolesa@gmail.com>
2022-07-01 18:48:28 -0700

    hexidecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int a;
+for (a = 48; a < 58; a++)
+{
+putchar(a)
+}
+for (a = 97; a <= 102; a++)
+{
+putchar(a);
+}
+putchar(10);
+return (0);
+}
