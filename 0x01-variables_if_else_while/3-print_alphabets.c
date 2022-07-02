773b7ee6107f3763799f078cd01484281758bdb0
isaactls <isacktolesa@gmail.com>
2022-07-01 17:45:05 -0700

    alphabet lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char a;
+for (a = 97; a <= 122; a++)
+{
+putchar(a);
+}
+putchar(10);
+char (a = 65; a <= 90; a++)
+putchar(a);
+return (0);
+}
