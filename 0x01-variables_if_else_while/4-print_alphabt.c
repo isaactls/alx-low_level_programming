c4f31e12bb78d1a6a63d3577a70b96dafe8aefd9
isaactls <isacktolesa@gmail.com>
2022-07-01 18:07:11 -0700

    skip q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char a;
+for (a = a; a <= z; a++)
+{
+if (a !0= q && a != e)
+{
+putchar(a);
+}
+
+putchar(10);
+}
+return (0);
+}
