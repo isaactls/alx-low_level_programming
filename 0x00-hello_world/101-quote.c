18b2b712eb6ae3d3fa6f7acb19bdb2f7bc9dbdb3
isaactls <isacktolesa@gmail.com>
2022-07-04 16:36:55 -0700

    no printf or put function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19, 58\n);
+return (0);
+}
