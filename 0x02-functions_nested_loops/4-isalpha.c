19a5f2e6e93e39987348ff462107dc8adb6894b1
isaactls <isacktolesa@gmail.com>
2022-07-06 09:11:54 -0700

    fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha: entry point
+ * _isalpha - entry point
  * @c: variable for character to be checked
  *
  * return: 0 or 1
- **/
+ */
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
