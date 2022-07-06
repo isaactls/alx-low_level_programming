216129860cf9ae45d12347743088ea045711371f
isaactls <isacktolesa@gmail.com>
2022-07-05 19:08:52 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * print alphabet: 'print alphabet in lowercase'
+ *
+ * return: no return (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int b;
+for (b = 1; b <=10; b++)
+{
+int a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+}
+_putchar('\n');
+}
