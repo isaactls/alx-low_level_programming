bba24050d2b28005b01085990d744e4c0c0096d0
isaactls <isacktolesa@gmail.com>
2022-07-05 18:23:56 -0700

    please betty leave me alone

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 int a;
