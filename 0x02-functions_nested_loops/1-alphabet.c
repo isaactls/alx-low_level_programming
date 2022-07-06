1c3dc6c0ead37335e54246cc160cbe75ca20c366
isaactls <isacktolesa@gmail.com>
2022-07-05 17:52:27 -0700

    ok

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,13 @@
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
 int a;
-for (a = 97; a <= 122; a++)
+for (a = 'a'; a <= 'z'; a++)
 {
 _putchar(a);
 }
-_putchar(10);
+_putchar('\n');
 }
